fd538574308d43dc49a9124900ed8740c291017c
KeisukeYamashita <19yamashita15@gmail.com>
2021-01-03 15:33:50 +0900

    Implement v1 action
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,34 @@
 #!/bin/sh -l
 
 set -eu
-set -o pipefail
 
-PIXELA_POST_BASE_URL="https://pixe.la/v1/users"
+PIXELA_POST_BASE_URL="https://pixe.la/v1"
 
 main() {
     username="${1}"
-    graph="${2}"
-    quantity="${3}"
-    date="${4}"
+    token="${2}"
+    graph="${3}"
+    quantity="${4}"
+    date="${5}"
 
     if [ -z "${username}" ]; then
         username="${PIXELA_USERNAME}"
     fi
 
     if [ -z "${date}" ]; then
-        date=$(date +%s)
+        date=$(date +%Y%m%d)
     fi
 
+    echo "[DEBUG] Post metrics for username:${username}, graph:${graph}, quantity:${quantity}, date:${date}"
+
     curl \
-        -X POST "${PIXELA_POST_BASE_URL}/${username}/graphs/${graph}" \
-        -H "X-USER-TOKEN:${PIXELA_TOKEN}" \
+        -s \
+        -H "X-USER-TOKEN:${token}" \
+        -X POST "${PIXELA_POST_BASE_URL}/users/${username}/graphs/${graph}" \
         -d @- <<EOF
 {
-    "date": ${date},
-    "quantity": ${quantity}
+    "date": "${date}",
+    "quantity": "${quantity}"
 }
 EOF
 }
