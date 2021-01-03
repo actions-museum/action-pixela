8bcba926b00cfc25573bb3f750e83a2f99410015
KeisukeYamashita <19yamashita15@gmail.com>
2021-01-03 15:17:40 +0900

    Initial commit
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,33 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -eu
+set -o pipefail
+
+PIXELA_POST_BASE_URL="https://pixe.la/v1/users"
+
+main() {
+    username="${1}"
+    graph="${2}"
+    quantity="${3}"
+    date="${4}"
+
+    if [ -z "${username}" ]; then
+        username="${PIXELA_USERNAME}"
+    fi
+
+    if [ -z "${date}" ]; then
+        date=$(date +%s)
+    fi
+
+    curl \
+        -X POST "${PIXELA_POST_BASE_URL}/${username}/graphs/${graph}" \
+        -H "X-USER-TOKEN:${PIXELA_TOKEN}" \
+        -d @- <<EOF
+{
+    "date": ${date},
+    "quantity": ${quantity}
+}
+EOF
+}
+
+main "$@"
